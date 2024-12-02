a3b8fdf63c0106a18928b60f39b4c68727c4c1c7
Joji Kuriyathan Joy <101832298+joji-kuriyathan-joy@users.noreply.github.com>
2024-12-02 18:25:34 +0000

    Music App Version 2
    More and clean design for Web and Mobile. 
    Use of Gird display

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function show_album_view(album_id){
+  window.location.href = `./album_view.html?id=${album_id}`;
+}
+   
+function show_artist(artist_id){
+    window.location.href = `./artist.html?id=${artist_id}`;
+  }
+     
